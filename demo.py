cf65508b10c6bdd7ce32942b0122475a99678992
Paulk528 <64648652+Paulk528@users.noreply.github.com>
2025-10-06 18:53:47 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,3 +4,12 @@ st.title("MY PROJECT")
 st.header("This is a header")
 st.subheader("This is a subheader")
 st.text("I love AI VIET NAM")
+
+
+import streamlit as st
+
+st.write('I love AI VIET NAM')
+st.write('## Heading 2')
+st.write('$ \\sqrt{2x+2} $')
+st.write('1 + 1 = ', 2)
+
