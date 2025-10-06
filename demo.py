6a2ac2e06cfb0347036be484962ee2238aa81435
Paulk528 <64648652+Paulk528@users.noreply.github.com>
2025-10-06 18:54:51 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,3 +13,31 @@ st.write('## Heading 2')
 st.write('$ \\sqrt{2x+2} $')
 st.write('1 + 1 = ', 2)
 
+
+import streamlit as st
+
+def get_name():
+    st.write("Thai")
+
+agree = st.checkbox("I agree", on_change=get_name)
+
+if agree:
+    st.write("Great!")
+
+st.radio(
+    "Your favorite color:",
+    ['Yellow', 'Blue'],
+    captions=['Vàng', 'Xanh']
+)
+
+
+import streamlit as st
+
+option = st.selectbox(
+    "Your contact:",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+st.write("Selected:", option)
+
+
